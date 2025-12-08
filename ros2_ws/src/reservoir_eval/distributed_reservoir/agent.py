01352a49911adfbc737840d5ca815d3eaa8c028e
--global <--global>
2025-12-07 18:57:23 -0800

    stuf and things

--- ros2_ws/src/reservoir_eval/distributed_reservoir/agent.py
+++ ros2_ws/src/reservoir_eval/distributed_reservoir/agent.py
@@ -42,7 +42,7 @@ class Agent_ROSNode(Node):
         # Subscribe to Edge before creating publishers
         # Init Subscribers  
         # get params
-        self.reservoir_param_subscriber = self.create_subscription(String, 'reservoir_params', self._handle_params)
+        self.reservoir_param_subscriber = self.create_subscription(dict, 'reservoir_params', self._handle_params)
         # subscribe to reservoir output
         self.data_subscription = self.create_subscription(Float32MultiArray, f'{func}_res_msg', self._handle_reservoir_data, queue_size = 10)
         
@@ -72,15 +72,19 @@ class Agent_ROSNode(Node):
         try:
             reservoir_output = self._msg_to_layer(msg)
             results = self.model(reservoir_output)
-            # RESULTS GETS PASSED TO LOGGER
+            # TODO :: RESULTS GETS PASSED TO LOGGER
         except Exception as e:
+            ### In _handle_params you do if self.logger is None
+            ### If it can be not initialized then it is technically
+            ### possible for it to not be initialized at any point
+            ### Maybe a function that checks and returns the logger??
             self.get_logger().error(f"Error handling reservoir data: {e}")
 
     def _handle_params(self, msg):
         '''
         callback to handle parameter updates from edge
         '''
-        if self.logger is None:
+        if self.logger is None: ### You use logger like 6 other times and you dont do this there
             self.logger = Logger(self.function, params, self.get_logger())
         self.get_logger().info(f"Received parameters: {msg.data}")
         self.connected_to_edge = True
@@ -96,7 +100,7 @@ class Agent_ROSNode(Node):
         '''
         translate data to ROS msg
         '''
-        msg = Float32MultiArray()
+        msg = Float32MultiArray() # is this required?
         msg.data = np.asarray(u, dtype=np.float32).flatten().tolist()
         return msg
     
@@ -113,7 +117,10 @@ class Agent_ROSNode(Node):
         tspan = np.linspace(t0, self.msg_length*self.dt, self.msg_length)
         sol = solve_ivp(self.func, [t0, self.msg_length*self.dt], ic, t_eval=tspan, method=self.integrator)
         return sol.y, sol.t
-
+    
+    ### This is fine but technically inconsistent with your naming scheme
+    ### Are these functions to meant be exposed? 
+    ### Python convention is _ before function name for "private" internal functions
     def send_batch_to_res(self,t_curr):
         '''
         generate data and send batch of data to 
@@ -123,12 +130,15 @@ class Agent_ROSNode(Node):
         self.data_hist.append(data)
         self.time_hist.append(time)
         # Convert output to ROS message and send
+        ### Why is this 2 functions at this point anyways?
+        ### Send Data is a single line anyways
         msg = self._data_to_msg(data)
         self._send_data(msg)
         # Update initial conditions
         self.ic = data[:, -1]  # Last state becomes next initial condition
         self.get_logger().debug(f"Generated and sent data batch at t=[{t_curr},{t_curr+self.msg_length*self.dt})")
 
+    ###same as send_batch_to_res
     def run(self):
         '''
         Main node spin loop: generates data, feeds through reservoir, sends output
