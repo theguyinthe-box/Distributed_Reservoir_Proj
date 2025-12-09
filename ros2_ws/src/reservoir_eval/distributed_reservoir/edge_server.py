3b947d7b57cd3561bd6c3618141d1f874d4fffcc
--global <--global>
2025-12-08 17:21:58 -0800

    did a bunch of stuff, need to start testing

--- ros2_ws/src/reservoir_eval/distributed_reservoir/edge_server.py
+++ ros2_ws/src/reservoir_eval/distributed_reservoir/edge_server.py
@@ -11,35 +11,40 @@ import torch.nn as nn
 # codebase imports
 from reservoir import Reservoir
 from functions import dynamical_functions as d
-from logger import Logger
 
 class Edge_ROSNode(Node):
-    def __init__(self, func:str, 
-                 res_dim):
+    def __init__(self, 
+                 func:str, 
+                 res_dim = 500,
+                 spectral_radius = 1.0,
+                 leak_rate = 0.15,
+                 iterations = 20):
+        
         super().__init__('edge_server_ros_node')    
+        
         #hyper parameters
-
         self.reservoir_params = {
             "input_dim": d.function_dims(func), 
-            "res_dim": 500,
-            "spectral_radius": 1.3,
-            "leak_rate": 0.15, 
+            "res_dim": res_dim,
+            "spectral_radius": spectral_radius,
+            "leak_rate": leak_rate,
+            "iter": iterations
         }
 
         self.runtime_params = {
             "model_path": "/ros2_ws/model.pt",
         }
         
-        self.param_publisher = self.create_publisher(dict, )
+        self.param_publisher = self.create_publisher(dict,'reservoir_params', queue_size = 10)
 
         # Publishers / Subscribers
-        self.subscription = self.create_subscription(Float64MultiArray, f'{func}_agent_msg', self.handle_input, queue_size = 10)
-        self.publisher = self.create_publisher(Float64MultiArray, f'{func}_res_msg', queue_size = 10)
-
+        self.agent_subscription = self.create_subscription(Float64MultiArray, f'{func}_agent_msg', self._handle_input, queue_size = 10)
+        self.output_publisher = self.create_publisher(Float64MultiArray, f'{func}_res_msg', queue_size = 10)
+        
         self.get_logger().info(f"Edge reservoir node ready and waiting for input.")
 
         # instantiate reservoir 
-        self.model = Reservoir(self.reservoir_params('res_dim'), self.reservoir_params('sepctral_radius'), self.reservoir_params('leak_rate'))
+        self.model = Reservoir(res_dim, spectral_radius=spectral_radius, leak_rate = leak_rate)
 
     def _msg_to_layer(self, msg):
         '''
@@ -47,7 +52,7 @@ class Edge_ROSNode(Node):
         tranlate to nodes
         '''
         return torch.tensor(msg.data, dtype=torch.float32, device=self.device)
-    
+   
     def _data_to_msg(self, u):
         '''
         translate data to ROS msg
@@ -56,18 +61,18 @@ class Edge_ROSNode(Node):
         msg.data = np.asarray(u, dtype=np.float32).flatten().tolist()
         return msg
     
-    def _send_data(self, msg):
-        '''
-        send data to edge network
-        '''
-        self.data_publisher.publish(msg)
-    
+    def _handle_input(self, msg):
+
+        input_data = self._msg_to_layer(msg)
+        output_data = self.model.forward(input_data, self.reservoir_params('iter'))
+        output_msg = self._data_to_msg(output_data)
+        self.output_publisher(output_msg)
+
 
     '''
-    TODO
+    TODO in later iterations
     function to handle new connections
     function to queue to incoming messages
     function to take from queue to reservoir and output 
-    function to 
     '''    
     
