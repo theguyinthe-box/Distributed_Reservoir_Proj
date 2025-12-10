2b0fec95ff3d8ac4503cefe566c410f3ca6875a2
--global <--global>
2025-12-09 20:52:12 -0800

    add package.xml for ros

--- docker/entrypoints/edge_entrypoint.sh
+++ docker/entrypoints/edge_entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
 source /ros2_ws/install/setup.bash
-ros2 run distributed_reservoir edge
+export PYTHONPATH=/ros2_ws/install/distributed_reservoir/lib/python3.10/site-packages:$PYTHONPATH
+ros2 run distributed_reservoir edge_server
